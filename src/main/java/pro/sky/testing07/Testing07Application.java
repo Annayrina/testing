efb7ef796febd6b1afae71a44aef05b1361e93c9
Anna <annayrina1998@mail.ru>
2024-04-07 23:20:25 +0700

    первый коммит

---
+++ src/main/java/pro/sky/testing07/Testing07Application.java
@@ -0,0 +1,13 @@
+package pro.sky.testing07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testing07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testing07Application.class, args);
+	}
+
+}
